064efec42971b774b5bd7435573dd3204b4d46b0
Tomasz Rybarczyk <paluho@gmail.com>
2017-01-09 23:03:26 +0100

    Fix dependency links

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages
 from setuptools import setup
 
 REQUIREMENTS = [
-    'python-lua-ast == 0.0.2',
+    'python-lua-ast == 0.0.4',
 ]
 
 DEPENDENCY_LINKS = [
-    'git+git://github.com/paluh/python-lua-ast.git@0.0.2#egg=python-lua-ast-0.0.2',
+    'https://github.com/paluh/python-lua-ast/archive/0.0.4.zip#egg=python-lua-ast-0.0.4'
 ]
 
 setup(
